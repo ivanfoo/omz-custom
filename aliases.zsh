a3c29ec5ce3b3074e9e98bbbd8336270983f9b33
ivanfoo <sanfru.ivan@gmail.com>
2015-06-13 15:21:30 +0200

    Adding few aliases and gitignore

---
+++ aliases.zsh
@@ -0,0 +1,12 @@
+# My aliases
+
+#Misc
+alias igrep='grep -i'
+alias tailf='tail -f'
+
+#Dirs aliases
+projects=$HOME/projects
+
+# Editing and reloading aliases
+alias add_alias='vi $HOME/.oh-my-zsh/custom/my-aliases.zsh'
+alias reload='source $HOME/.zshrc'
