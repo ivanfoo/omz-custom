bdd5845fdaf2882b6ab5df2d65f8103d3357697c
ivanfoo <sanfru.ivan@gmail.com>
2015-07-07 18:01:59 +0200

    Some aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,19 +1,28 @@
-#### My aliases ####
-
-# Misc
-alias igrep='grep -i'
-alias tailf='tail -f'
-alias bed-time='sudo shutdown -h now'
+# Making my life easier
 
 # Dirs aliases
-p=~/projects
+pro=~/projects
+gopro=~/go
 omz=~/.oh-my-zsh
+
+# tgz shortcuts
+alias tczf='tar czfv $1 $2'
+alias txzf='tar xzfv $1' 
+
+# Fuck off stuff
+alias untmp='rm -rf /tmp/*'
+alias undown='rm -rf ~/Downloads/*'
+
+# Misc
+alias gri='grep -i'
+alias tf='tail -f'
+alias bed='sudo shutdown -h now'
 
 # Editing and reloading aliases
 alias cheat='vi ~/.oh-my-zsh/custom/aliases.zsh'
-alias load_aliases='source ~/.oh-my-zsh/custom/aliases.zsh'
+alias docheat='source ~/.oh-my-zsh/custom/aliases.zsh'
 
 # Editing and reloading zsh config
-alias zcheat='vi ~/.zshrc'
+alias cfg='vi ~/.zshrc'
 alias reload='source ~/.zshrc' 
 
